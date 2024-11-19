2bbd60a27732174a63ef6d0c9d6abf8c2395d34c
kirillburdin <kitillburdin76@gmail.com>
2024-11-19 23:28:51 +0300

    добавил бд и репозиторий вместо мапы

--- MyProducts/src/main/java/com/example/myproducts/models/Product.java
+++ MyProducts/src/main/java/com/example/myproducts/models/Product.java
@@ -1,5 +1,6 @@
 package com.example.myproducts.models;
 
+import jakarta.persistence.*;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -8,16 +9,28 @@ import lombok.*;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
 public class Product {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @NotNull
-    @Size(max = 255)
+
+    @Size(max = 255, message = "Название товара должно быть не более 255 символов")
+    @Column(nullable = false, length = 255)
     private String name;
-    @Size(max = 4094)
+
+    @Size(max = 4096, message = "Описание товара должно быть не более 4096 символов")
+    @Column(length = 4096)
     private String description;
-    @Min(0)
+
+    @Min(value = 0, message = "Цена товара не может быть меньше 0")
+    @Column(nullable = false)
     private double price;
+
+    @Column(nullable = false)
     private boolean inStock = false;
 
 }
+
+
